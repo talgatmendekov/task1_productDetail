d0435993a306d0fb882cf6585c55f26cbd93c16c
talgatmendekov <talgatmendekov@gmail.com>
2024-07-21 20:31:53 +0600

    feat: set up Apollo Client.

--- src/index.js
+++ src/index.js
@@ -2,15 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 
-
+const client = new ApolloClient({
+  uri: 'https://api.uno.kg/graphql/',
+  cache: new InMemoryCache()
+})
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>
+  </ApolloProvider>
 );
 
